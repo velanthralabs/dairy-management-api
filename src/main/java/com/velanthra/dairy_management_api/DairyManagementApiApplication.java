45fd442c09c6a05d2f971fbee23b4d0dcdac78da
Poovarasan <poovarasan.muralidaran@outlook.com>
2026-02-17 22:31:46 +0530

    Initial spring boot setup - JAVA 21

---
+++ src/main/java/com/velanthra/dairy_management_api/DairyManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.velanthra.dairy_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DairyManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DairyManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/velanthra/dairy_management_api/DairyManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.velanthra.dairy_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DairyManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
