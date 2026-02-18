ad5691c022eeb3d56aebd6b6f544ceef87d1cccd
atharsh07 <atharsh0425@gmail.com>
2026-02-19 00:46:40 +0530

    first commit

--- src/main/java/com/velanthra/dairy_management_api/DairyManagementApiApplication.java
+++ src/main/java/com/velanthra/dairy_management_api/DairyManagementApiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DairyManagementApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DairyManagementApiApplication.class, args);
+		System.out.println("Atharsh");
 	}
 
 }
