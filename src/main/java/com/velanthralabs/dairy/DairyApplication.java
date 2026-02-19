acac1c637a986784c587e75a63dfbc60a42e0afb
Poovarasan <poovarasan.muralidaran@outlook.com>
2026-02-19 22:24:50 +0530

    Timezone and property file commit.

--- src/main/java/com/velanthralabs/dairy/DairyApplication.java
+++ src/main/java/com/velanthralabs/dairy/DairyApplication.java
@@ -3,10 +3,13 @@ package com.velanthralabs.dairy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class DairyApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(DairyApplication.class, args);
 	}
 
